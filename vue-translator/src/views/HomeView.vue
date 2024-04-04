d28223bab46e541bc5ddd653a8dcdcbd2da6796e
vala <vala@utb.cz>
2024-04-04 10:46:04 +0200

    complete commit

---
+++ vue-translator/src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+
+const userInput = ref("");
+const transResult = ref("");
+
+const translate = () => {
+  console.log(userInput.value);
+
+  fetch(`https://api.mymemory.translated.net/get?q=${userInput.value}&langpair=en|cs`)
+  .then( response => response.json())
+  .then( data => {
+    // tady mame odpoved ze serveru v promenne data
+    console.log(data);
+    transResult.value = data.responseData.translatedText
+  });
+}
+
+</script>
+
+<template>
+  <main>
+    <input type="text" v-model="userInput" placeholder="Text for translation..." >
+    <button @click="translate">Translate</button>
+    <div>
+        {{ userInput }}
+        -->
+        {{ transResult }}
+    </div>
+  </main>
+</template>
+
+<style>
+  input {
+    color: blueviolet;
+  }
+</style>
